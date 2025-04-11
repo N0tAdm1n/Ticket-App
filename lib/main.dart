43570d415a07a11d426b4febde23369e0f6872d4
N0tAdm1n <n0tadm1n29@gmail.com>
2025-04-11 15:53:26 +0530

    Connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:test_as/firebase_options.dart';
 import 'package:test_as/pages/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
